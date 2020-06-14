a9439306f0542e43e0172d590dcb6a57dce5b4f9
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-06-13 21:50:04 -0400

    Revert "starting pz"
    This reverts commit 44b4770985e689d4fa982f777b83a95c67830725.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+//import "./js101/index";
 import React from "react";
-import ReactDOM from "react-dom"; //eslint-disable-line
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-const element = <h1>Patient Zero</h1>;
-console.log(element);
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
