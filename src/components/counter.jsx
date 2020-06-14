16697c4a61b2ce94083696ba333d06624455b980
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-06-13 22:50:57 -0400

    conditionally rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,32 +6,17 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"],
   };
 
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
+    return this.state.tags.map((tag, index) => <li key={index}>{tag}</li>);
+  }
+
   render() {
     return (
       <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-nm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag, index) => (
-            <li key={index}>{tag}</li>
-          ))}
-        </ul>
+        <ul>{this.renderTags()}</ul>
       </React.Fragment>
     );
-  }
-
-  getBadgeClasses() {
-    let classes = ["badge", "m-2"];
-    return (this.state.count === 0
-      ? [...classes, "badge-warning"]
-      : [...classes, "badge-primary"]
-    ).join(" ");
-  }
-
-  formatCount() {
-    const { count } = this.state;
-
-    return count === 0 ? "Zero" : count;
   }
 }
 
