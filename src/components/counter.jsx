08a555fa2ba7bbbcc79d59df5e6a1c6bf97dc8e6
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-06-13 22:04:02 -0400

    basic component

---
+++ src/components/counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Patient Zero</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counter;
