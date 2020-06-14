47fbe79fb4177395ca4a74cf00b38c87aac26c7e
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-06-14 16:26:04 -0400

    stateless functional component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,42 +1,40 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  handleNew = () => {
-    console.log("new clicked");
-    this.props.onNew();
-  };
+const handleNew = (props) => {
+  console.log("new clicked");
+  props.onNew();
+};
 
-  handleReset = () => {
-    console.log("reset clicked");
-    this.props.onReset();
-  };
+const handleReset = (props) => {
+  console.log("reset clicked");
+  props.onReset();
+};
 
-  render() {
-    return (
-      <nav class="navbar navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-          <span className="m-2">Total</span>
-          <span className="badge badge-pill badge-secondary">
-            {this.props.counters
-              .map((counter) => counter.value)
-              .reduce(
-                (accumulator, currentValue) => accumulator + currentValue,
-                0
-              )}
-          </span>
-          <button className="btn-primary m-2" onClick={() => this.handleNew()}>
-            Add New Counter
-          </button>
-          <button
-            className="m-2 button-danger"
-            onClick={() => this.handleReset()}
-          >
-            Reset
-          </button>{" "}
-        </a>
-      </nav>
-    );
-  }
-}
+const NavBar = (props) => {
+  return (
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <span className="m-2">Total</span>
+        <span className="badge badge-pill badge-secondary">
+          {props.counters
+            .map((counter) => counter.value)
+            .reduce(
+              (accumulator, currentValue) => accumulator + currentValue,
+              0
+            )}
+        </span>
+        <button className="btn-primary m-2" onClick={() => handleNew(props)}>
+          Add New Counter
+        </button>
+        <button
+          className="m-2 button-danger"
+          onClick={() => handleReset(props)}
+        >
+          Reset
+        </button>{" "}
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
