f0429d889f0030b7f77812b204cef59744f19d24
Martin Perry <bobthearsonist@users.noreply.github.com>
2020-06-13 22:20:37 -0400

    bootstrap design elements

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,8 +9,8 @@ class Counter extends Component {
     const { imageUrl } = this.state;
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-nm">Increment</button>
       </React.Fragment>
     );
   }
